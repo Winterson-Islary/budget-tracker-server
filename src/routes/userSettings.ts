849dcfab2f069481a3f5cdf3bc84e2c41dee49b8
Winterson <u19cse1028@cit.ac.in>
2024-05-13 23:39:52 +0530

    db user settings insert/delete

--- src/routes/userSettings.ts
+++ src/routes/userSettings.ts
@@ -1,13 +1,8 @@
 import { Hono } from "hono";
 import { getAuth } from "@hono/clerk-auth";
-
-const UserSettingHardCoded = {
-	data: {
-		userId: "testUserFromServer",
-		currency: "USD",
-	},
-};
-let Currency: string | undefined = "USD";
+import { db } from "../db/db.ts";
+import { userSettings as userSettingsTable } from "../db/schemas/schema.ts";
+import { eq } from "drizzle-orm";
 
 export const userSettings = new Hono()
 	.get("/", async (ctx) => {
@@ -17,17 +12,37 @@ export const userSettings = new Hono()
 				message: "You are not logged in.",
 			});
 		}
+		const settings = await db
+			.select()
+			.from(userSettingsTable)
+			.where(eq(userSettingsTable.userId, auth.userId));
+
 		return ctx.json({
 			message: "You are logged in.",
-			currency: Currency,
+			settings: settings[0],
 			userId: auth.userId,
 		});
 	})
 	.post("/", async (ctx) => {
+		const auth = getAuth(ctx);
+		if (!auth?.userId) {
+			return ctx.json({
+				message: "You are not logged in.",
+			});
+		}
 		const body = await ctx.req.json();
-		console.log(body);
-		Currency = body.currency;
-		return ctx.json({ message: "Successfully Completed Update" });
+		const entry_exist = await db.query.userSettings.findFirst({
+			where: eq(userSettingsTable.userId, auth.userId),
+		});
+		if (entry_exist) {
+			await db
+				.update(userSettingsTable)
+				.set({ currency: body.currency })
+				.where(eq(userSettingsTable.userId, auth.userId));
+			return ctx.json({ message: "Updated Currency" });
+		}
+		await db
+			.insert(userSettingsTable)
+			.values({ userId: auth.userId, currency: body.currency });
+		return ctx.json({ message: "Successfully Completed Setting Currency" });
 	});
-// .delete
-// .put
