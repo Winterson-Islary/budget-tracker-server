3e831611cdc8c07e21fefd14328f826264d1273f
Winterson <u19cse1028@cit.ac.in>
2024-06-03 23:37:32 +0530

    add transcation schema type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -6,3 +6,14 @@ export const CreateCategorySchema = z.object({
 	type: z.enum(["income", "expense"]),
 });
 export type CreateCategorySchemaType = z.infer<typeof CreateCategorySchema>;
+
+export const CreateTransactionSchema = z.object({
+	amount: z.coerce.number().positive().multipleOf(0.01),
+	description: z.string().optional(),
+	date: z.coerce.date(),
+	category: z.string(),
+	type: z.union([z.literal("income"), z.literal("expense")]),
+});
+export type CreateTransactionSchemaType = z.infer<
+	typeof CreateTransactionSchema
+>;
